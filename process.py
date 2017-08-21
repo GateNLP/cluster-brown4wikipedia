99622e8c2f1b7a8ee90899ad879174fc148f8c9b
Johann Petrak <johann.petrak@gmail.com>
2017-08-21 20:56:19 +0200

    Script to prepare the input for the clustering: add separator tokens for document boundaries and sentence boundaries and remove one or two-token sentences.

---
+++ process.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import sys
+from nltk.tokenize.moses import MosesTokenizer
+
+tokenizer = MosesTokenizer()
+## replace a line which only contains </doc> with <SEP_DOC/>
+## remove a line which starts with "<doc"
+## replace empty lines with "<SEP_SENT/>"
+## remove non-empty lines with 0 or one spaces (one or two tokens) 
+n = 0
+hadsep = False
+for line in sys.stdin:
+    n = n + 1
+    if n % 10000 == 0:
+        print("Lines processed: ",n,file=sys.stderr)
+    line = line.strip()
+    if len(line) == 0:
+        if not hadsep: print("\n<SEP_SENT/>")
+        hadsep = True
+    elif line.startswith("<doc"):
+        pass
+    elif line.startswith("</doc"):
+        print("\n<SEP_DOC/>")
+    elif line.count(" ") < 2:
+        pass
+    else:
+        ## do some additional tokenisation 
+        line = tokenizer.tokenize(line,return_str=True)
+        print(line,sep='',end='')
+        hadsep = False
